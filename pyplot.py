4437eafb7f1cca0ada76df17df11c6a3d8604401
Andreas Weh <andreas.weh@student.uni-augsburg.de>
2017-03-09 16:30:34 +0100

    Set up `argparser` to use scripts as parent.

--- pyplot.py
+++ pyplot.py
@@ -1,12 +1,29 @@
-"""Module to bundle plotting scripts"""
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+"""Module to bundle plotting scripts
+
+`activate-global-python-argcomplete` must be run to enable auto completion """
 
 import argparse
+import argcomplete
+import plotter
+
 
 
 def parse_arguments():
     """Argument Parser, providing available scripts"""
     parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(
+        title = 'plotter',
+        description = 'available plotting scripts'
+    )
+    import plotter.plotn
+    test = subparsers.add_parser('plotn', parents=[plotter.plotn.parse_arguments()], help=plotter.plotn.__doc__.split('\n',1)[0])
+    configure = subparsers.add_parser('configure', help='configure this script.')
+    # parser.add_argument('--largetest')
+    # parser.add_argument('program', choices=('test1', 'test2'))
 
+    argcomplete.autocomplete(parser)
     args = parser.parse_args()
     return args
 
