a41dcc266d0218800d7b9f23be59ad5bbedee3ac
Andreas Weh <andreas.weh@student.uni-augsburg.de>
2017-03-16 10:45:23 +0100

    Extract function to add parsers to subparsers.

--- pyplot.py
+++ pyplot.py
@@ -3,60 +3,73 @@
 """Module to bundle plotting scripts
 
 `activate-global-python-argcomplete` must be run to enable auto completion """
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 import argparse
 import argcomplete
 import plotter
-from configure import (get_parser as conf_get_parser,
-                       main as conf_main)
+import os
+import os.path
+import configure
+
+SCRIPT_DIR = 'plotter'
+
+
+# def _help(arg, dirname, fnames):
+#     print((dirname.replace(os.sep, '.')))
+#     return (dirname.replace(os.sep, '.'))
+
+# os.path.walk('plotter', _help, None)
+# def register_subparsers(subparsers, parser):
+#     subparsers[name].add_subparsers(
+#         title=str(parser)
+#     )
 
 
 def get_parser():
     """Return Argument Parser, providing available scripts"""
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(
-        title='plotter',
+        title=SCRIPT_DIR,
         description='available plotting scripts',
         dest='used_subparser',
     )
     for module_str in plotter.__all__:
-        module = __import__('plotter.' + module_str, fromlist=module_str)
-        try:
-            module_subparser = subparsers.add_parser(
-                module_str, parents=(module.get_parser(add_help=False),),
-                description=module.__doc__,
-                help=module.__doc__.split('\n', 1)[0]
-            )
-            module_subparser.set_defaults(run=module.main)
-        except AttributeError:
-            # module doesn't provide `get_parser`
-            module_subparser = subparsers.add_parser(
-                module_str,
-                description=module.__doc__,
-                help=module.__doc__.split('\n', 1)[0],
-            )
-            module_subparser.add_argument(
-                'arguments',
-                nargs=argparse.REMAINDER,
-                help='possible unknown arguments for {module}'.format(module=module_str),
-            )
-            module_subparser.set_defaults(run=substitute, name=module_str, main=module.main)
+        module = __import__(SCRIPT_DIR + '.' + module_str, fromlist=module_str)
+        register_parser(subparsers, module_str, module)
 
-    configure = subparsers.add_parser(
-        'configure',
-        parents=(conf_get_parser(add_help=False),),
-        description=module.__doc__,
-        help='configure this script.',
-    )
-    configure.set_defaults(run=conf_main)
+    register_parser(subparsers, 'configure', configure)
 
     argcomplete.autocomplete(parser)
     return parser
 
 
+def register_parser(subparsers, module_str, module):
+    """register `module` to `subparsers`"""
+    try:
+        module_subparser = subparsers.add_parser(
+            module_str, parents=(module.get_parser(add_help=False),),
+            description=module.__doc__,
+            help=module.__doc__.split('\n', 1)[0]
+        )
+        module_subparser.set_defaults(run=module.main)
+    except AttributeError:
+        # module doesn't provide `get_parser`
+        module_subparser = subparsers.add_parser(
+            module_str,
+            description=module.__doc__,
+            help=module.__doc__.split('\n', 1)[0],
+        )
+        module_subparser.add_argument(
+            'arguments',
+            nargs=argparse.REMAINDER,
+            help='possible unknown arguments for {module}'.format(module=module_str),
+        )
+        module_subparser.set_defaults(run=substitute, name=module_str, main=module.main)
+
+
 def substitute(args):
-    """replace `sys.argv` to launch script without 'get_parser'"""
+    """replace `sys.argv` to launch a script without 'get_parser'"""
     import sys
     replace_argv = [args.name,] + args.arguments
     sys.argv = replace_argv
